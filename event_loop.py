e0373ceebe06e609644a054f457f6723f350f6cc
Sam Thorold <sam.thorold@gmail.com>
2024-12-13 15:33:30 +0000

    Init

---
+++ event_loop.py
@@ -0,0 +1,64 @@
+"""PyEv Python Discrete Event Loop."""
+
+from collections import deque
+from dataclasses import dataclass
+import logging
+from typing import Protocol
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(kw_only=True, frozen=True)
+class Event:
+    t: int
+
+
+class ProcessProtocol(Protocol):
+    def act(self, event: Event) -> list[Event]: ...
+
+
+class EventLoop:
+    def __init__(
+        self,
+        processes: list[ProcessProtocol],
+        events: list[Event] | None = None,
+        current_timestep: int = 0,
+    ) -> None:
+        self.processes = processes
+        self.events: deque[Event] = deque()
+        for event in events or []:
+            self.add_event(event)
+        self.current_timestep = current_timestep
+
+    def add_event(self, event: Event) -> None:
+        logger.debug("Adding event %r", event)
+        self.events.insert(event.t + 1, event)
+
+    def peek_event(self) -> Event | None:
+        if len(self.events) > 0:
+            return self.events[0]
+
+    def next_event(self) -> Event | None:
+        if event := self.peek_event():
+            if event.t > self.current_timestep:
+                return None
+            return self.events.popleft()
+
+    def tick(self) -> bool:
+        logger.debug("Ticking at %d", self.current_timestep)
+        while event := self.next_event():
+            logger.debug("Broadcasting event %r", event)
+            for process in self.processes:
+                for event in process.act(event):
+                    self.add_event(event)
+        if event := self.peek_event():
+            self.current_timestep = event.t
+            return True
+        logger.debug("No events to process")
+        return False
+
+    def run(self) -> None:
+        while self.tick():
+            pass
+
--- main.py
+++ main.py
@@ -1,68 +1,24 @@
 """PyEv Python Discrete Event Loop."""
 
-
-from collections import deque
 from dataclasses import dataclass
+from event_loop import Event, EventLoop
 import logging
-from typing import Protocol
 
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass(kw_only=True, frozen=True)
-class Event:
-    t: int
+class MyEv(Event):
+    a: str
 
 
-class ProcessProtocol(Protocol):
+class Process:
     def act(self, event: Event) -> list[Event]:
-        ...
-
-
-class EventLoop:
-    def __init__(
-        self,
-        processes: list[ProcessProtocol],
-        events: list[Event] | None = None,
-        current_timestep: int = 0,
-    ) -> None:
-        self.processes = processes
-        self.events: deque[Event] = deque()
-        for event in events or []:
-            self.add_event(event)
-        self.current_timestep = current_timestep
-
-    def add_event(self, event: Event) -> None:
-        logger.debug("Adding event %r", event)
-        self.events.insert(event.t + 1, event)
-
-    def peek_event(self) -> Event | None:
-        if len(self.events) > 0:
-            return self.events[0]
-
-    def next_event(self) -> Event | None:
-        if event := self.peek_event():
-            if event.t > self.current_timestep:
-                return None
-            return self.events.popleft()
-
-    def tick(self) -> bool:
-        logger.debug("Ticking at %d", self.current_timestep)
-        while event := self.next_event():
-            logger.debug("Broadcasting event %r", event)
-            for process in self.processes:
-                for event in process.act(event):
-                    self.add_event(event)
-        if event := self.peek_event():
-            self.current_timestep = event.t
-            return True
-        logger.debug("No events to process")
-        return False
-
-    def run(self) -> None:
-        while self.tick():
-            pass
+        logger.debug("Acting on event %r", event)
+        if event.t >= 10:
+            return []
+        return [MyEv(t=event.t + 1, a="b" if (event.t % 2) else "c")]
 
 
 if __name__ == "__main__":
@@ -70,19 +26,6 @@ if __name__ == "__main__":
     import os
 
     logging.basicConfig(level=os.environ.get("PYEV_LOG_LEVEL") or logging.DEBUG)
-
-    @dataclass(kw_only=True, frozen=True)
-    class MyEv(Event):
-        a: str
-
-    class Process:
-        def act(self, event: Event) -> list[Event]:
-            logger.debug("Acting on event %r", event)
-            if event.t >= 10:
-                return []
-            return [
-                MyEv(t=event.t + 1, a="b" if (event.t % 2) else "c")
-            ]
 
     loop = EventLoop(
         processes=[Process()],
